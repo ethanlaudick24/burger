7c8ebc1ed63a0c3dc4513b93ec5066b3b4fbb3ae
Ethan <ethan.laudick24@d155.org>
2022-10-24 08:26:11 -0500

    Initial Commit

---
+++ burger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  burger
+//
+//  Created by ETHAN LAUDICK on 10/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
